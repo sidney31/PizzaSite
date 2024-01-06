7665d3ff7ebffb73aabbcb59ee4c52d58a44f688
sidney31 <surovbogdan05@gmail.com>
2024-01-06 21:20:48 +0300

    start project

---
+++ PizzaSite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-wrp(2@ug*a&iyzqy5efpp-w4zkn^2pt&g)8-5wd&8!hn3v7b3p"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
