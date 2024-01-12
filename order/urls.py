fe456f841b180fef59abdbe415e72d6ad9fd99e1
sidney31 <surovbogdan05@gmail.com>
2024-01-13 00:43:30 +0300

    change views for add/remove dishes in basket; Create template for manage dishes in basket

--- order/urls.py
+++ order/urls.py
@@ -6,5 +6,6 @@ app_name = 'order'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/<int:dish_id>/', views.add_dish_to_basket, name='add_dish_to_basket'),
-    path('remove/<int:dish_id>/', views.remove_dish_from_basket, name='remove_dish_from_basket'),
+    path('remove/<int:dish_id>', views.remove_dish_from_basket, name='remove_dish_from_basket'),
+    path('full_remove/<int:dish_id>', views.full_remove_dish_from_basket, name='full_remove_dish_from_basket'),
 ]
--- order/views.py
+++ order/views.py
@@ -15,23 +15,42 @@ def index(request):
 
 def add_dish_to_basket(request, dish_id):
     basket = Basket.objects.get(id=1)
-    dish_in_basket = DishInBasket.objects.create(dish_id=dish_id)
-    old_total_price = Basket.objects.get(id=1).total_price
+    dish_in_basket = DishInBasket.objects.filter(dish_id=dish_id).first()
+    if dish_in_basket:
+        dish_in_basket.quantity += 1
+        dish_in_basket.save()
+    else:
+        dish_in_basket = DishInBasket.objects.create(dish_id=dish_id)
 
-    basket.total_price = old_total_price + dish_in_basket.dish.price
+    basket.total_price += dish_in_basket.dish.price
     basket.save()
 
-    return redirect('menu')
+    return redirect(request.META.get('HTTP_REFERER'))
 
 
 def remove_dish_from_basket(request, dish_id):
     basket = Basket.objects.get(id=1)
     dish_in_basket = DishInBasket.objects.filter(dish_id=dish_id).first()
-    old_total_price = Basket.objects.get(id=1).total_price
 
-    basket.total_price = old_total_price - dish_in_basket.dish.price
+    basket.total_price -= dish_in_basket.dish.price
     basket.save()
 
+    if dish_in_basket.quantity == 1:
+        basket.total_price -= dish_in_basket.dish.price * dish_in_basket.quantity
+        dish_in_basket.delete()
+    else:
+        basket.total_price -= dish_in_basket.dish.price
+        dish_in_basket.quantity -= 1
+        dish_in_basket.save()
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
+def full_remove_dish_from_basket(request, dish_id):
+    basket = Basket.objects.get(id=1)
+    dish_in_basket = DishInBasket.objects.filter(dish_id=dish_id).first()
+
+    basket.total_price -= dish_in_basket.dish.price * dish_in_basket.quantity
     dish_in_basket.delete()
 
-    return redirect('order:index')
+    return redirect(request.META.get('HTTP_REFERER'))
